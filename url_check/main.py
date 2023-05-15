9af36d3a9e7615f93209dd24266139ff3a5075fe
ntnguyen1234 <thanhnguyen.nguyen94@gmail.com>
2023-05-15 13:09:37 +0700

    update url_check

--- url_check/main.py
+++ url_check/main.py
@@ -3,18 +3,35 @@ from time import sleep
 
 
 def main():
-    urls = [
-        'https://otx.alienvault.com/indicator/domain/',
+    base_urls = [
+        'https://transparencyreport.google.com/safe-browsing/search?url=',
         'https://safeweb.norton.com/report/show?url=',
         'https://sitecheck.sucuri.net/results/',
         'https://www.siteadvisor.com/sitereport.html?url=',
-        'https://www.urlvoid.com/scan/'
     ]
-    domain = input('>>> ')
-    for url in urls:
-        new_url = f'{url}{domain}'
+    base_domains = [
+        'https://otx.alienvault.com/indicator/domain/',
+        'https://opentip.kaspersky.com/',
+        'https://www.virustotal.com/gui/domain/',
+        'https://transparencyreport.google.com/safe-browsing/search?url=',
+        'https://safeweb.norton.com/report/show?url=',
+        'https://sitecheck.sucuri.net/results/',
+        'https://www.siteadvisor.com/sitereport.html?url=',
+        'https://www.urlvoid.com/scan/',
+        'https://yandex.com/safety/?l10n=en&url='
+    ]
+    test_url = input('>>> ')
+    for url in base_urls:
+        new_url = f'{url}{test_url}'
         print(new_url)
         run(['powershell', f'start "{new_url}"'])
+        sleep(1)
+
+    test_domain = test_url.split('://')[1].split('/')[0].replace('www.', '')
+    for domain in base_domains:
+        new_domain = f'{domain}{test_domain}'
+        print(new_domain)
+        run(['powershell', f'start "{new_domain}"'])
         sleep(1)
 
 
