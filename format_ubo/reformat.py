b6410f13637d11ae5947169f6dd61dfca1b83671
ntnguyen1234 <thanhnguyen.nguyen94@gmail.com>
2023-06-10 18:33:53 +0700

    remove print

--- format_ubo/reformat.py
+++ format_ubo/reformat.py
@@ -69,7 +69,6 @@ class Reformatter:
                 case 'cosmetic':
                     self.troubleshoot[key][k] = [f'##{item}'.replace('####', '##') for item in extract.split(' ##')]
                 case 'network':
-                    print(extract)
                     self.troubleshoot[key][k] = get_search(r'([-.\w]+): (.+?(?=(?: [-.\w]+:|$)))', extract)
                 case _:
                     self.troubleshoot[key][k] = int(extract) if extract.isnumeric() else extract
