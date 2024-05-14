2f4d128b37ea1a7884fff02a2a65cd6ec678d2b9
ntnguyen1234 <thanhnguyen.nguyen94@gmail.com>
2024-05-14 13:41:56 +0700

    update browser method

--- url_check/main.py
+++ url_check/main.py
@@ -1,8 +1,14 @@
+import json
 from subprocess import run
 from time import sleep
 
 
 def main():
+    with open('data.json', 'r') as fr:
+        data = json.load(fr)
+
+    browser_path = data['browser_path']
+
     base_urls = [
         'https://transparencyreport.google.com/safe-browsing/search?url=',
         'https://safeweb.norton.com/report/show?url=',
@@ -24,14 +30,24 @@ def main():
     for url in base_urls:
         new_url = f'{url}{test_url}'
         print(new_url)
-        run(['powershell', f'start "{new_url}"'])
+        run([
+            'powershell', 
+            '&', f'"{browser_path}"'
+            '-p', data['profile'], # For Firefox
+            f'"{new_url}"'
+        ])
         sleep(1)
 
     test_domain = test_url.split('://')[1].split('/')[0].replace('www.', '')
     for domain in base_domains:
         new_domain = f'{domain}{test_domain}'
         print(new_domain)
-        run(['powershell', f'start "{new_domain}"'])
+        run([
+            'powershell', 
+            '&', f'"{browser_path}"'
+            '-p', data['profile'], # For Firefox
+            f'"{new_domain}"'
+        ])
         sleep(1)
 
 
